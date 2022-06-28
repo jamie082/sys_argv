e8571029fcd159b697b28d2740fcfe36cf196f5a
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-06-28 09:37:09 -0700

    added file

---
+++ sys_argv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys, getopt
+
+name1 = sys.argv[1]
+name2 = sys.argv[2]
+name3 = sys.argv[3]
+
+def main():
+    if opt == '-h':
+        print ("This is the -h")
+
+def read_file(filename):
+    with open(filename) as file:
+        while True:
+            line = file.readline()
+
+            if len(line) == 0:
+                break
+
+            print(line)
+
+files = sys.argv[1:]
+
+for filename in files:
+    read_file(filename)
+
+print("hello", name1,",",name2," and ", name3)
+
