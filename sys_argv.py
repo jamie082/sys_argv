4a5a089bee8b85363516e8420326539f5bde6d56
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-06-28 22:55:50 -0700

    added first working commit

--- sys_argv.py
+++ sys_argv.py
@@ -2,29 +2,31 @@
 import sys
 import getopt
 
-def main():
-
-    try:
-        opts, args = getopt.getopt(args, "hi:o:", ["--input", "--output"])
-    except getopt.GetoptError:
-        print ("sys_argv.py --input <inputfile> --user <user_id> --output <outputfile>")
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print ("sys_argv.py --input <inputfile> --user <user_id> --output <outputfile>")
-            print ("This is the -h")
-            sys.exit()
-
-        elif opt in ("-i", "--input"):
-            arg_input = arg
-        elif opt in ("-u", "--user"):
-            arg_user = arg
-        elif opt in ("-o", "--output"):
-            arg_output = arg
-
-"""
-print("hello", name1,',',name2,' and ', name3)
+args = sys.argv[1:]
+arg_input = ''
+arg_user = ''
+arg_output = ''
 
 
-myfunct(sys.argv)
+try:
+    opts, args = getopt.getopt(args, "hi:o:", ["--input", "--output"])
+except getopt.GetoptError:
+    print ("sys_argv.py --input <inputfile> --user <user_id> --output <outputfile>")
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+       print ("sys_argv.py --input <inputfile> --user <user_id> --output <outputfile>")
+       print ("This is the -h")
+       sys.exit()
+
+    elif opt in ("-i", "--input"):
+       arg_input = arg
+    elif opt in ("-u", "--user"):
+       arg_user = arg
+    elif opt in ("-o", "--output"):
+       arg_output = arg
+
+print ("Input file is ", arg_input)
+print ("User mod is ", arg_user)
+print ("Output file is ", arg_output)
