5b538668a607b96e48b6c9e3aa5bf62d7595fc65
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-06-28 21:44:20 -0700

    Added files

--- sys_argv.py
+++ sys_argv.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-import sys, getopt
+import sys
+import getopt
 
 name1 = sys.argv[1]
 name2 = sys.argv[2]
 name3 = sys.argv[3]
 
-def main():
+def myfunct(argv):
     if opt == '-h':
         print ("This is the -h")
+
+    elif opt in ("-i", "--input"):
+        arg_input = arg
+    elif opt in ("-u", "--user");
+        arg_user = arg
+    elif opt in ("-o", "--output"):
+        arg_output arg
 
 def read_file(filename):
     with open(filename) as file:
