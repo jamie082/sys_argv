c4d98614cc11d34d912ab7e16e9265d144ee4f2f
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-07-15 22:07:51 -0700

    updated source

--- sys_argv.py
+++ sys_argv.py
@@ -20,13 +20,13 @@ for opt, arg in opts:
        print ("sys_argv.py --input <inputfile> --user <user_id> --output <outputfile>")
        sys.exit()
 
-    elif opt in ("-i", "--input"):
+   # python3 flags 
+    if opt in ("-i", "--input"):
+       print ("Do this, ",  arg_input)
        arg_input = arg
     elif opt in ("-u", "--user"):
        arg_user = arg
+       print (arg_user)
     elif opt in ("-o", "--output"):
        arg_output = arg
-
-print ("Input file is ", arg_input)
-print ("User mod is ", arg_user)
-print ("Output file is ", arg_output)
+       print (arg_output)
