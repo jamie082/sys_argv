bd6d920cc97845b0538f5f7177ab63e27b027fc0
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-07-19 22:13:35 -0700

    updated user id mod

--- sys_argv.py
+++ sys_argv.py
@@ -10,7 +10,7 @@ arg_output = ''
 add = 0.0
 n = len(sys.argv)
 try:
-    opts, args = getopt.getopt(args, "i:u:o", ["--input", "--u", "--output"])
+    opts, args = getopt.getopt(args, "hi:u:o", ["--input", "--user", "--output"])
 except getopt.GetoptError:
     print ("sys_argv.py --input <inputfile> --user <user_id> --output <outputfile>")
     sys.exit(2)
@@ -27,7 +27,7 @@ for opt, arg in opts:
        print ("Input file is ", arg_input)
     elif opt in ("-u", "--user"):
        arg_user = arg
-       print ("Input file is ", arg_user)
+       print ("User id is ", arg_user)
     elif opt in ("-o", "--output"):
        arg_output = arg
        print ("Output file is ", arg_output)
